82960c2d33370986f235ffc911d746b89a22ecd7
WendolynQuartey <quarteywendolyn@gmail.com>
2026-04-14 15:33:10 -0400

    added suffix

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -7,15 +7,39 @@ public class FullNameParser {
             System.out.print("Please enter your full name:");
             String fullName = scanner.nextLine();
 
-        String[] nameSections = fullName.split("\\s");
+        String[] nameSections = fullName.split(" ");
 
-        if (nameSections.length == 2){
-            System.out.println("First Name:" + nameSections[0]);
-            System.out.println("Last Name:" + nameSections[1]);
-        } else if (nameSections.length == 3){
-            System.out.println("First Name:" + nameSections[0]);
-            System.out.println("Middle Name:" + nameSections[1]);
-            System.out.println("Last Name:" + nameSections[2]);
+        if (fullName.contains(",")) {
+            if (nameSections.length == 3){
+                System.out.println("First Name: " + nameSections[0]);
+                System.out.println("Middle Name: (none)");
+                System.out.println("Last Name: " + nameSections[1].replace(",", " "));
+
+                System.out.println("Suffix: " + nameSections[2]);
+            } else if (nameSections.length == 4) {
+                System.out.println("First Name:" + nameSections[0]);
+                if (nameSections[1].length() == 2){
+                    System.out.println("Middle Initial: " + nameSections[1]);
+                } else {
+                    System.out.println("Middle Name: " + nameSections[1]);
+                }
+                System.out.println("Last Name: " + nameSections[2].replace(",", " "));
+                System.out.println("Suffix: " + nameSections[3]);
+            }
+        } else {
+            if (nameSections.length == 2) {
+                System.out.println("First Name: " + nameSections[0]);
+                System.out.println("Middle Name: (none)");
+                System.out.println("Last Name: " + nameSections[1]);
+            } else if (nameSections.length == 3){
+                System.out.println("First Name: " + nameSections[0]);
+                if (nameSections[1].length() == 2){
+                    System.out.println("Middle Initial: " + nameSections[1]);
+                } else {
+                    System.out.println("Middle Name: " + nameSections[1]);
+                }
+                System.out.println("Last Name:" + nameSections[2]);
+            }
         }
 
     }
